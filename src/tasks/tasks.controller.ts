cad1c0989f55c925cc9a324d8283dc798ed68d50
Joel Garcia <joel.garciajr84@gmail.com>
2020-05-20 16:35:15 -0300

    Added controller and service

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+
+@Controller('tasks')
+export class TasksController {
+
+    constructor(private taskService: TasksService){}
+    @Get()
+    getAllTasks() {
+        return this.taskService.getAllTasks();
+    }
+
+}
---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class TasksService {
+
+    private tasks = [];
+
+    getAllTasks(){
+        return this.tasks;
+    }
+}
