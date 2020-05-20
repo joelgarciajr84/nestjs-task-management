07d1da9583ab3df8cc2776817ee127a9025389fd
Joel Garcia <joel.garciajr84@gmail.com>
2020-05-20 19:36:48 -0300

    Fix uuid call and add insert task method

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,13 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Task } from './tasks.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
 
-    constructor(private taskService: TasksService){}
+    constructor(private readonly taskService: TasksService){}
     @Get()
-    getAllTasks() {
+    public getAllTasks(): Array<Task>{
         return this.taskService.getAllTasks();
+    }
+
+    @Post()
+    public createTask(
+        @Body('title') title: string,
+        @Body('description') description: string,
+
+    ){
+       return this.taskService.createTask(title, description);
+
     }
 
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,11 +1,24 @@
 import { Injectable } from '@nestjs/common';
-
+import { v4 as uuidv4 } from 'uuid';
+import { Task, TaskStatus } from './tasks.model';
 @Injectable()
 export class TasksService {
 
-    private tasks = [];
+    private readonly tasks: Array<Task> = [];
 
-    getAllTasks(){
+    public getAllTasks(): Array<Task>{
         return this.tasks;
+    }
+
+    public createTask(title: string, description: string): Task {
+        const task: Task = {
+            id: uuidv4(),
+            title,
+            description,
+            status: TaskStatus.OPEN
+        };
+        
+        this.tasks.push(task);
+        return task;
     }
 }
