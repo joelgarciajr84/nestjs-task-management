887d22f8e76c99dc7a8d6d3e438e32956b393140
Joel Garcia <joel.garciajr84@gmail.com>
2020-05-21 11:11:11 -0300

    New entity and repository

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,24 +1,22 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
-import { Task } from './tasks.model';
+// tslint:disable: comment-format
+import { Body, Controller, Get, Param, Post, ParseIntPipe, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Task } from './task.entity';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
 
     constructor(private readonly taskService: TasksService){}
-    @Get()
-    public getAllTasks(): Array<Task>{
-        return this.taskService.getAllTasks();
+
+    @Get('/:id')
+    public async getTaskById(@Param('id', ParseIntPipe) id: number): Promise<Task> {
+        return this.taskService.getTaskById(id);
     }
 
     @Post()
-    public createTask(
-        @Body('title') title: string,
-        @Body('description') description: string,
-
-    ){
-       return this.taskService.createTask(title, description);
-
+    @UsePipes(ValidationPipe)
+    public async createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task>{
+        return this.taskService.createTask(createTaskDto);
     }
 
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,24 +1,55 @@
-import { Injectable } from '@nestjs/common';
-import { v4 as uuidv4 } from 'uuid';
-import { Task, TaskStatus } from './tasks.model';
+// tslint:disable: comment-format
+
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Task } from './task.entity';
+import { TaskRepository } from './task.repository';
+import { TaskStatus } from './tasks-status-enum';
+
 @Injectable()
 export class TasksService {
-
-    private readonly tasks: Array<Task> = [];
-
-    public getAllTasks(): Array<Task>{
-        return this.tasks;
+    constructor(@InjectRepository(TaskRepository)
+        private readonly taskRepository: TaskRepository
+    ){
+        
     }
 
-    public createTask(title: string, description: string): Task {
-        const task: Task = {
-            id: uuidv4(),
-            title,
-            description,
-            status: TaskStatus.OPEN
-        };
-        
-        this.tasks.push(task);
+    public async getTaskById(id: number): Promise<Task> {
+        const found = await this.taskRepository.findOne(id);
+
+        if(!found){
+            throw new NotFoundException(`Task with ID ${id} not found`);
+        }
+
+        return found;
+    }
+
+    public async createTask(createTaskDto: CreateTaskDto): Promise<Task>{
+        const task: Task = new Task();
+        const { title, description } = createTaskDto;
+        task.title = title;
+        task.description = description;
+        task.status = TaskStatus.OPEN;
+        await task.save();
         return task;
     }
+    // private readonly tasks: Array<Task> = [];
+
+    // public getAllTasks(): Array<Task>{
+    //     return this.tasks;
+    // }
+
+
+
+    // public createTask(title: string, description: string): Task {
+    //     const task: Task = {
+    //         id: uuidv4(),
+    //         title,
+    //         description,
+    //         status: TaskStatus.OPEN
+    //     };
+        
+    //     this.tasks.push(task);
+    //     return task;
+    // }
 }
