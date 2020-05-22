cc906904d31dd1edb7d67960af8ea49616059d30
Joel Garcia <joel.garciajr84@gmail.com>
2020-05-22 00:17:10 -0300

    Added auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 import { typeOrmConfig } from './config/typeorm.config';
 import { TasksModule } from './tasks/tasks.module';
 @Module({
   imports: [
+    AuthModule,
     TypeOrmModule.forRoot(typeOrmConfig),
     TasksModule,
   ],
 })
-export class AppModule {}
+export class AppModule { }
