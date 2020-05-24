b8cb0d08afaca56b363722bf0f2ba30f1de5be29
Joel Garcia <joel.garciajr84@gmail.com>
2020-05-24 13:40:44 -0300

    Added swagger

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,12 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsIn, IsNotEmpty, IsOptional } from 'class-validator';
 import { TaskStatus } from '../tasks-status-enum';
 
 export class GetTasksFilterDto {
+    @ApiProperty()
     @IsOptional()
     @IsIn([TaskStatus.OPEN, TaskStatus.IN_PROGRESS, TaskStatus.DONE])
     public status: TaskStatus;
 
-    @IsOptional()
+    @ApiProperty()
+    @IsOptional()   
     @IsNotEmpty()
     public search: string;
 }
