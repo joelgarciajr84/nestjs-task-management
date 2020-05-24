739f683177f51ddda7fe72311eeae6f0ca8970c8
Joel Garcia <joel.garciajr84@gmail.com>
2020-05-24 13:11:41 -0300

    added env vars

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
 import { AuthModule } from './auth/auth.module';
 import { typeOrmConfig } from './config/typeorm.config';
 import { TasksModule } from './tasks/tasks.module';
+
+dotenv.config();
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env'],
+}),
     AuthModule,
     TypeOrmModule.forRoot(typeOrmConfig),
     TasksModule,
   ],
 })
-export class AppModule { }
-
+export class AppModule {}
